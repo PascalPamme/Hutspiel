0d96591973ab1c4e481f7e22cd8574f36b0a629f
PascalPamme <PascalPamme@gmx.de>
2022-05-16 08:35:04 +0200

    Game finished and tested

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 console.log("Willkommen zum grossartigen Hutspiel! Zu Beginn erhaelst du zunaechst 3 Muenzen. Errate unter welchem Hut sich der Ball befindet und du erhälst eine weitere Muenze. Ratest du allerdings falsch verlierst eine Muenze. Das Spiel ist vorbei sobald du keine Muenzen mehr uebrig hast ")
 var readlineSync = require('readline-sync');
-//var coins = 3;
 
-function hideBallUnderCup(){
-  
-  var randomNumber = Math.floor(Math.random() * 2);
-  return String(randomNumber);
-}
+//function for random String generator A-C (a-c)
+function hideBallUnderCup(length = 1) {
+  var result           = '';
+  var characters       = "ABCabc";
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+ }
+
+// Coin wallet and overall game mechanics
 var coins = 3;
 function playGame(){
   var currentposition = hideBallUnderCup();
   var input = readlineSync.question("Unter Welchem Hut befindet sich der Ball nun? Tippe A, B oder C und druecke enter");
-  //console.log(typeof input, input);
-  //console.log(typeof currentposition, currentposition)
   if (currentposition === input) {
     console.log("Glueckwunch! Der Ball war unter dem richtigen Hut");
   }
@@ -28,7 +32,7 @@ function playGame(){
     coins--
     
   }
-console.log("Du hast noch " + coins + "Muenzen");
+console.log("Du hast noch " + coins + " Muenzen");
 if (coins === 0) {
   console.log("Game Over")
 }
@@ -39,21 +43,8 @@ while (coins >= 1) {
   playGame();
 };
 
-//playGame();
 
-/*if (readlineSync.keyInYN(currentposition)) {
-    
-    console.log("Glueckwunch! Der Ball war unter dem richtigen Hut");
-   
-  } else {
-    
-    console.log('Leider keine Muenze fuer dich');
 
-  }
-  function currentposition(){
-    var currentposition = hideBallUnderCup();
-    console.log(currentposition);
-  }*/
 
 
 
