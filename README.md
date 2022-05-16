b3d8fe1245f9f057203f7805a007de1fe72a3995
PascalPamme <PascalPamme@gmx.de>
2022-05-16 16:57:57 +0200

    eslint added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hutspiel
-The super Hutspiel
+
+Um das spiel zu spielen:
+'''
+$ npm install
+'''
+Um das spiel zu starten
+'''
+$ npms start
+'''
--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-console.log("Willkommen zum grossartigen Hutspiel! Zu Beginn erhaelst du zunaechst 3 Muenzen. Errate unter welchem Hut sich der Ball befindet und du erhälst eine weitere Muenze. Ratest du allerdings falsch verlierst eine Muenze. Das Spiel ist vorbei sobald du keine Muenzen mehr uebrig hast ")
+console.log("Willkommen zum grossartigen Hutspiel! Zu Beginn erhaelst du zunaechst 3 Muenzen. Errate unter welchem Hut sich der Ball befindet und du erhälst eine weitere Muenze. Ratest du allerdings falsch verlierst eine Muenze. Das Spiel ist vorbei sobald du keine Muenzen mehr uebrig hast ");
 var readlineSync = require('readline-sync');
 
 //function for random String generator A-C (a-c)
 function hideBallUnderCup(length = 1) {
-  var result           = '';
-  var characters       = "ABCabc";
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
- }
+    var result           = '';
+    var characters       = "ABCabc";
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 // Coin wallet and overall game mechanics
 var coins = 3;
 function playGame(){
-  var currentposition = hideBallUnderCup();
-  var input = readlineSync.question("Unter Welchem Hut befindet sich der Ball nun? Tippe A, B oder C und druecke enter");
-  if (currentposition === input) {
-    console.log("Glueckwunch! Der Ball war unter dem richtigen Hut");
-  }
-  else {
-    
-    console.log('Leider keine Muenze fuer dich');
-  }
+    var currentposition = hideBallUnderCup();
+    var input = readlineSync.question("Unter Welchem Hut befindet sich der Ball nun? Tippe A, B oder C und druecke enter\n");
+    if (currentposition === input) {
+        console.log("Glueckwunch! Der Ball war unter dem richtigen Hut");
+    }
+    else {
+        console.log("Der Ball war unter Hut " + currentposition);
+        console.log('Leider keine Muenze fuer dich');
+    }
   
-  if (currentposition === input) {
-    coins++
-  }
-  else {
-    coins--
+    if (currentposition === input) {
+        coins++;
+    }
+    else {
+        coins--;
     
-  }
-console.log("Du hast noch " + coins + " Muenzen");
-if (coins === 0) {
-  console.log("Game Over")
-}
+    }
+    console.log("Du hast noch " + coins + " Muenzen\n");
+    if (coins === 0) {
+        console.log("Game Over");
+    }
 }
 
 
 while (coins >= 1) {
-  playGame();
-};
+    playGame();
+}
 
 
 
